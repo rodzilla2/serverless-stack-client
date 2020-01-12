ec24a96e0dd6ff93e3bdfba9e4d3f7f1cd096a25
Roddey Whitener <rwhitener@scentair.com>
2020-01-12 11:42:38 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-uploads0"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://18sm6y96t6.execute-api.us-east-1.amazonaws.com/prod/"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_g9TK2I3wu",
+      APP_CLIENT_ID: "2cstav4gd09vmh7nakpbgqps09",
+      IDENTITY_POOL_ID: "us-east-1:5d5e8dcf-85f9-407a-a025-6ccaf0f880a7"
+    }
+  };
